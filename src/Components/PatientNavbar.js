f7a987833a208c0daef7ed0de5e7690cce8d0248
Nikhil Mittal <mittalnikhil1123@gmail.com>
2022-04-26 20:25:25 +0530

    work on patDash

--- src/Components/PatientNavbar.js
+++ src/Components/PatientNavbar.js
@@ -3,44 +3,26 @@ import { Link } from 'react-router-dom'
 
 export default function PatientNavbar() {
 	return (
-		<>
-			<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" rel="stylesheet" />
-			<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-			<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-			<nav aria-label="breadcrumb">
-				<ol class="breadcrumb my-2">
-					<li class="breadcrumb-item active"><a href="#">New</a></li>
-					<li class="breadcrumb-item active"><a href="#">In-Review</a></li>
-					<li class="breadcrumb-item"><a>Interview</a></li>
-					<li class="breadcrumb-item"><a>Offered</a></li>
-				</ol>
-			</nav>
-
-			{/* <ol className="breadcrumb">
-		<li><a href="#">Home</a></li>
-		<li><a href="#">Library</a></li>
-		<li className="active">Data</li>
-	  </ol> */}
-
-			<div className="row">
-				<div className="col-3">
-					<div className="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-						<a className="nav-link active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Home</a>
-						<a className="nav-link" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Profile</a>
-						<a className="nav-link" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Messages</a>
-						<a className="nav-link" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Settings</a>
-					</div>
-				</div>
-				<div className="col-9">
-					<div className="tab-content" id="v-pills-tabContent">
-						<div className="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">...</div>
-						<div className="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">...</div>
-						<div className="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">...</div>
-						<div className="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">...</div>
-					</div>
-				</div>
-			</div>
-
-		</>
+    <>
+    <nav className="navbar navbar-expand-lg navbar-dark shadow rounded" style={{ backgroundColor: '#F2EEE3' }}>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/"><img src="navbar_image.png" className="img-fluid" width="200"></img>
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse flex-row-reverse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link active" to="/adminDash"><button type="button" className="btn btn-primary shadow" style={{ borderRadius: '12px', backgroundColor: '#00ADEF', borderColor: '#00ADEF' }} >HOME</button></Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link active" to="/"><button type="button" className="btn btn-primary shadow" style={{ borderRadius: '12px', backgroundColor: '#00ADEF', borderColor: '#00ADEF' }} >LOGOUT</button></Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </>
 	)
 }

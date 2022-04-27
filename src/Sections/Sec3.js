90974b8c2dd138ca1252256b7c250063953ce284
Nikhil Mittal <mittalnikhil1123@gmail.com>
2022-04-27 18:36:54 +0530

    hulalalala

---
+++ src/Sections/Sec3.js
@@ -0,0 +1,158 @@
+import React from 'react'
+
+export default function Sec3() {
+  return (
+    <>
+        <div className="container mb-5">
+            <div className="row">
+                <div className="col-12">
+                    <p class="fw-bold">3. Which of the following sentences is correct</p>
+                    <div>
+                        <input type="radio" name="box" id="one" />
+                        <input type="radio" name="box" id="two" />
+                        <input type="radio" name="box" id="three" />
+                        <input type="radio" name="box" id="four" />
+                        <label for="one" class="box first">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="two" class="box second">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="three" class="box third">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="four" class="box forth">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>  
+        </div>
+
+        <div className="container mb-5">
+            <div className="row">
+                <div className="col-12">
+                    <p class="fw-bold">1. Which of the following sentences is correct</p>
+                    <div>
+                        <input type="radio" name="box" id="one" />
+                        <input type="radio" name="box" id="two" />
+                        <input type="radio" name="box" id="three" />
+                        <input type="radio" name="box" id="four" />
+                        <label for="one" class="box first">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="two" class="box second">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="three" class="box third">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="four" class="box forth">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>  
+        </div>
+
+        <div className="container mb-5">
+            <div className="row">
+                <div className="col-12">
+                    <p class="fw-bold">1. Which of the following sentences is correct</p>
+                    <div>
+                        <input type="radio" name="box" id="one" />
+                        <input type="radio" name="box" id="two" />
+                        <input type="radio" name="box" id="three" />
+                        <input type="radio" name="box" id="four" />
+                        <label for="one" class="box first">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="two" class="box second">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="three" class="box third">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                        <label for="four" class="box forth">
+                            <div class="course">
+                                <span class="circle"></span>
+                                <span class="subject">
+                                    When its raining ,people's umbrella are all you're going to
+                                    see from above
+                                </span>
+                            </div>
+                        </label>
+                    </div>
+                </div>
+            </div>  
+        </div>
+        </>
+
+  )
+}
